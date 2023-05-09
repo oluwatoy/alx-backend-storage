af92eff3d2f77a80208ee406fd02c3220733d9db
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2023-05-09 23:26:53 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+""" Nginx logs stored in MongoDB
 """
-Improve 12-log_stats.py by adding the top 10 of the most present IPs
-"""
+
+
 from pymongo import MongoClient
 
 
 def log_stats():
-    """ the collection nginx of the database logs:
+    """ Database: logs
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
@@ -26,16 +27,6 @@ def log_stats():
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
-    print("IPs:")
-    sorted_ips = logs_collection.aggregate(
-        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-         {"$sort": {"count": -1}}])
-    i = 0
-    for s in sorted_ips:
-        if i == 10:
-            break
-        print(f"\t{s.get('_id')}: {s.get('count')}")
-        i += 1
 
 
 if __name__ == "__main__":
